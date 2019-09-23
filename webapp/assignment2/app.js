5fb3290b69fb8029c9338ea51d84e8a152454b6b
ShotgunSajida <sajida.mohammad@northeastern.edu>
2019-09-22 19:06:08 -0700

    Added DB connection

--- webapp/assignment2/app.js
+++ webapp/assignment2/app.js
@@ -1,11 +1,26 @@
-var express = require('express');
-const app = express();
-
-var constants = require('./constants.js');
-
-const apis = require('./src/api/api');
-app.listen(constants.PORT, () => {
-  console.log(`server running on port ${constants.PORT}`)
-});
-
-app.use("/apis",apis);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+const api = require('./src/api/api');
+const dotenv = require('dotenv');
+const db = require('./src/db');
+dotenv.config();
+const PORT = process.env.PORT;
+app.use(bodyParser.json())
+app.use(
+ bodyParser.urlencoded({
+   extended: true,
+ })
+)
+app.listen(PORT, () => {
+ console.log(`App running on PORT ${PORT}.`);
+})
+app.get('/', (request, response) => {
+ console.log(request);
+ response.json({ info: 'Node.js, Express, and Postgres API' });
+})
+//app.get('/test_users', db.getUsers)
+//app.get('/test_users/:id', db.getUserById)
+app.post('/create', api.createUser);
+//app.put('/test_users/:id', db.updateUser)
+//app.delete('/test_users/:id', db.deleteUser)
--- webapp/assignment2/src/api/api.js
+++ webapp/assignment2/src/api/api.js
@@ -1,14 +1,31 @@
 var express = require('express');
-var  todos  = require('../db.js');
-const router = express.Router();
-
-
-router.get('/todos', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'Wohooo',
-    todos: todos
-  })
-});
-
-module.exports =  router;
+import { Client } from 'pg';
+const dotenv = require('dotenv');
+dotenv.config();
+const { DB_USER, DB_PASSWORD, DB_PORT,DB_SCHEMA } = process.env;
+console.log(DB_USER);
+const createUser = (request, response) => {
+ const connectionString = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_SCHEMA}`;
+const client = new Client(connectionString);
+ const { username, password, emailaddress, firstname, lastname } = request.body;
+ client.connect(function(err) {
+   if(err) {
+     return console.error('could not connect to postgres', err);
+   }
+   client.query(
+     'INSERT INTO APPUSERS (username, password, emailaddress, firstname, lastname) VALUES ($1, $2, $3, $4, $5)', [username, password, emailaddress, firstname, lastname], function(err, result) {
+     if(err) {
+       return console.error('error running query', err);
+     }else{
+       response.json({ info: 'Inserted successfully' });
+     }
+   });
+  });
+}
+module.exports = {
+ //getUsers,
+ //getUserById,
+ createUser,
+ //updateUser,
+ //deleteUser,
+}
