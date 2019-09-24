f3a29d4efbe8c9f83532ddfce1a8d3d6044886c4
ShotgunSajida <sajida.mohammad@northeastern.edu>
2019-09-23 20:53:29 -0700

    added createUser, getUser, updateUser and new db schema

--- webapp/assignment2/app.js
+++ webapp/assignment2/app.js
@@ -8,19 +8,19 @@ dotenv.config();
 const PORT = process.env.PORT;
 app.use(bodyParser.json())
 app.use(
- bodyParser.urlencoded({
-   extended: true,
- })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 )
 app.listen(PORT, () => {
- console.log(`App running on PORT ${PORT}.`);
-})
+    console.log(`App running on PORT ${PORT}.`);
+});
 app.get('/', (request, response) => {
- console.log(request);
- response.json({ info: 'Node.js, Express, and Postgres API' });
+    console.log(request);
+    response.json({
+        info: 'Node.js, Express, and Postgres API'
+    });
 })
-//app.get('/test_users', db.getUsers)
-//app.get('/test_users/:id', db.getUserById)
-app.post('/create', api.createUser);
-//app.put('/test_users/:id', db.updateUser)
-//app.delete('/test_users/:id', db.deleteUser)
+app.post('/user/create', api.createUser);
+app.post('/user/get', api.getUser);
+app.post('/user/update', api.updateUser);
--- webapp/assignment2/src/api/api.js
+++ webapp/assignment2/src/api/api.js
@@ -1,31 +1,133 @@
-var express = require('express');
-import { Client } from 'pg';
+const {
+    Client
+} = require('pg');
 const dotenv = require('dotenv');
+const bcrypt = require('bcryptjs');
+
 dotenv.config();
-const { DB_USER, DB_PASSWORD, DB_PORT,DB_SCHEMA } = process.env;
-console.log(DB_USER);
+const {
+    DB_USER,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_SCHEMA
+} = process.env;
+
+const connectionString = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_SCHEMA}`;
+const database = new Client(connectionString);
+database.connect(function(err) {
+    if (err) {
+        console.error('could not connect to postgres', err);
+    } else {
+        console.log("successfully connected to postgres");
+    }
+});
+
 const createUser = (request, response) => {
- const connectionString = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_SCHEMA}`;
-const client = new Client(connectionString);
- const { username, password, emailaddress, firstname, lastname } = request.body;
- client.connect(function(err) {
-   if(err) {
-     return console.error('could not connect to postgres', err);
-   }
-   client.query(
-     'INSERT INTO APPUSERS (username, password, emailaddress, firstname, lastname) VALUES ($1, $2, $3, $4, $5)', [username, password, emailaddress, firstname, lastname], function(err, result) {
-     if(err) {
-       return console.error('error running query', err);
-     }else{
-       response.json({ info: 'Inserted successfully' });
-     }
-   });
-  });
+    const {
+        emailaddress,
+        password,
+        firstname,
+        lastname
+    } = request.body;
+    bcrypt.hash(password, 10, function(err, hash) {
+        database.query(
+            'INSERT INTO APPUSERS (emailaddress, password, firstname, lastname, account_created, account_updated) \
+      VALUES ($1, $2, $3, $4, $5, $6)', [emailaddress, hash, firstname, lastname, new Date(), new Date()],
+            function(err, result) {
+                if (err) {
+                    return response.status(500).send({
+                        error: 'Error creating user account'
+                    });
+                } else {
+                    return response.status(200).json({
+                        info: 'Inserted successfully'
+                    });
+                }
+            });
+    });
 }
+
+const getUser = (request, response) => {
+    const {
+        emailaddress
+    } = request.body;
+    database.query(
+        'SELECT emailaddress, firstname, lastname, account_created, account_updated from APPUSERS \
+      where emailaddress = $1', [emailaddress],
+        function(err, result) {
+            if (err) {
+                return response.status(500).send({
+                    error: 'Error getting user account'
+                });
+            } else {
+                return response.status(200).json(result.rows[0]);
+            }
+        });
+}
+
+const updateUser = (request, response) => {
+    const {
+        emailaddress,
+        firstname,
+        lastname,
+        password
+    } = request.body;
+    database.query(
+        'SELECT emailaddress, firstname, lastname from APPUSERS \
+      where emailaddress = $1', [emailaddress],
+        function(err, result) {
+            if (err) {
+                return response.status(500).send({
+                    error: 'Error getting user account'
+                });
+            } else {
+                let user = result.rows[0];
+                let update_firstname = firstname || user["firstname"];
+                let update_lastname = lastname || user["lastname"];
+                if (password != null && password != "") {
+                    console.log("Updating user with password");
+                    bcrypt.hash(password, 10, function(err, hash) {
+                        database.query("UPDATE APPUSERS SET firstname=$1, lastname=$2, password=$3, account_updated=$4 \
+                where emailaddress = $5",
+                            [update_firstname, update_lastname, hash, new Date(), emailaddress],
+                            function(err, result) {
+                                if (err) {
+                                    return response.status(500).send({
+                                        error: 'Error updating user account'
+                                    });
+                                } else {
+                                    console.info("successfully updated the user");
+                                    return response.status(200).json({
+                                        info: "success"
+                                    })
+                                }
+                            });
+                    });
+                } else {
+                    console.log("Updating user without password");
+                    database.query("UPDATE APPUSERS SET firstname=$1, lastname=$2, account_updated=$3 \
+            where emailaddress = $4",
+                        [update_firstname, update_lastname, new Date(), emailaddress],
+                        function(err, result) {
+                            if (err) {
+                                console.error("eror updating user", err);
+                                return response.status(500).send({
+                                    error: 'Error updating user account'
+                                });
+                            } else {
+                                console.info("successfully updated the user");
+                                return response.status(200).json({
+                                    info: "success"
+                                })
+                            }
+                        });
+                }
+            }
+        });
+}
+
 module.exports = {
- //getUsers,
- //getUserById,
- createUser,
- //updateUser,
- //deleteUser,
+    createUser,
+    getUser,
+    updateUser
 }
