f3ca0124e94c3fc63fa493c8c72d8572c21ff79e
Prajesh Jain <jain.pra@husky.neu.edu>
2019-09-25 17:13:12 -0400

    addding integration test and updated readme.md

--- webapp/assignment2/app.js
+++ webapp/assignment2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 const api = require('./src/api/api');
 const dotenv = require('dotenv');
 const db = require('./src/db');
@@ -23,3 +23,5 @@ app.listen(PORT, () => {
 app.post('/v1/user', api.createUser);
 app.put('/v1/user/self', api.updateUser);
 app.get('/v1/user/self', api.getUser);
+
+module.exports = app;
