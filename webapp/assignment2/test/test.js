e188b7606169ef0e63521f431c624dfd137ab64f
Prajesh Jain <43219343+JainPrajesh@users.noreply.github.com>
2019-09-25 22:10:04 -0400

    Assignment2 final (#6) (#6)
    * added boilerplate for userlogin
    
    * Added DB connection
    
    * modified .gitignore
    
    * modified .gitignore
    
    * added createUser, getUser, updateUser and new db schema
    
    * authorization
    
    * adding validations and authorization
    
    * added AuthPromise
    
    * updateUser with put and authentication
    
    * addding integration test and updated readme.md
    
    * adding test
    
    * Bug fixes and displaying correct error codes

---
+++ webapp/assignment2/test/test.js
@@ -0,0 +1,31 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+describe("Login", () => {
+    describe('/GET Login', () => {
+        it('it should validate user if correct', (done) => {
+            chai.request(server)
+                .get('/v1/user/self')
+                .set("Authorization", "basic " + new Buffer("bvkvikas@gmail.com:Vikas@1234").toString("base64"))
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        it('it should validate user if non correct, return unauthorized', (done) => {
+            chai.request(server)
+                .get('/v1/user/self')
+                .set("Authorization", "basic " + new Buffer("krish@gmail.com:krishna@123").toString("base64"))
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+});
