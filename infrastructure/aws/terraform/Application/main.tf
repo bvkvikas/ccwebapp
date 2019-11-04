4298ecbc849895c1b2e4e735d7b5f40ea3931c4b
Krishna Vikas <venkatakrishnavika.b@husky.neu.edu>
2019-11-03 23:03:00 -0500

    circleci debug

--- infrastructure/aws/terraform/Application/main.tf
+++ infrastructure/aws/terraform/Application/main.tf
@@ -408,8 +408,8 @@ resource "aws_instance" "EC2Instance" {
                       sudo yum install ruby -y
                       sudo yum install wget -y
                       cd /home/centos
-                      wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-                      chmod +x ./install
+                      # wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+                      # chmod +x ./install
                       sudo service codedeploy-agent status
                       sudo service codedeploy-agent start
                       sudo service codedeploy-agent status
