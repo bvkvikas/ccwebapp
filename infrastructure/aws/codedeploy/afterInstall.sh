a79e099019d5a19168e70c2852e3c79d62c3350b
ShotgunSajida <sajida.mohammad@northeastern.edu>
2019-11-05 00:06:14 -0800

    Add statsd client

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -13,5 +13,5 @@
 
 
 cd /home/centos/node-app/assignment2
-sudo npm install
+npm install | tee ./npm-install.log
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/node-app/assignment2/cloudwatch-agent-config.json -s
