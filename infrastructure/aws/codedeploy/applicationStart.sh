dfd81ad6db313c64a2fc3ade961319a72e5ec685
Prajesh Jain <jain.pra@husky.neu.edu>
2019-11-06 16:35:14 -0500

    adding changes

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -5,3 +5,4 @@ sudo cp ./RecipeOnTheGo.service /etc/systemd/system/RecipeOnTheGo.service
 sudo systemctl start RecipeOnTheGo
 sudo systemctl status RecipeOnTheGo
 echo "Started the service"
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/node-app/assignment2/cloudwatch-agent-config.json -s
