bf4bdc0443d5b034b7708b8dc452a0d914dea531
ShotgunSajida <sajida.mohammad@northeastern.edu>
2019-11-05 20:46:27 -0800

    exporting variables

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -11,15 +11,7 @@
 # sudo rm -rf /opt/tomcat/logs/*.log
 # sudo rm -rf /opt/tomcat/logs/*.txt
 
-echo host=${aws_db_instance.rds.address} >> .env
-echo RDS_CONNECTION_STRING=${aws_db_instance.rds.address} >> .env
-echo RDS_USER_NAME=thunderstorm >> .env
-echo RDS_PASSWORD=thunderstorm_123 >> .env
-echo RDS_DB_NAME=thunderstorm >> .env
-echo PORT=3005 >> .env
-echo S3_BUCKET_NAME=${var.bucketName} >> .env
-echo bucket=${var.codedeployS3Bucket} >> .env
-chmod 777 .env
+
 cd /home/centos/node-app/assignment2
 npm install > ./npm-install.log 2>&1
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/node-app/assignment2/cloudwatch-agent-config.json -s
