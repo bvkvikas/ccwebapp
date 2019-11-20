6f1a335ac832f2d01ddc7ef2acbe034a5e6eac10
Krishna Vikas <venkatakrishnavika.b@husky.neu.edu>
2019-11-19 22:48:21 -0500

    endpoint test

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -5,5 +5,5 @@ cd /home/centos/node-app/assignment2
 sudo cp ./RecipeOnTheGo.service /etc/systemd/system/RecipeOnTheGo.service
 sudo systemctl daemon-reload
 sudo systemctl start RecipeOnTheGo
-sudo systemctl status RecipeOnTheGo > /tmp/appstop.txt
+sudo systemctl status RecipeOnTheGo > /tmp/appstart.txt
 echo "Started the service"
